483d7571b4d9f1a506a91bf85bb8c57db8c06eac
Mert Yılmaz <myyilmaz97@gmail.com>
2024-01-04 19:40:58 +0300

    react icons want working installed expo icons

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,38 +1,66 @@
 import { Tabs } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
-const TabsLayout = () => {
-  return (
-    <Tabs>
-      <Tabs.Screen
-        name="coupons/index"
-        options={{
-          headerShown: false,
-          tabBarLabel: "Kuponlarım",
-        }}
-      />
-      <Tabs.Screen
-        name="home/index"
-        options={{
-          headerShown: false,
-          tabBarLabel: "Anasayfa",
-        }}
-      />
-      <Tabs.Screen
-        name="branches/index"
-        options={{
-          headerShown: false,
-          tabBarLabel: "Şubeler",
-        }}
-      />
-      <Tabs.Screen
-        name="profiles/index"
-        options={{
-          headerShown: false,
-          tabBarLabel: "Profilim",
-        }}
-      />
-    </Tabs>
-  );
-};
+const TabsLayout = () => (
+  <Tabs>
+    <Tabs.Screen
+      name="coupons/index"
+      options={{
+        headerShown: false,
+        tabBarLabel: "Kuponlarım",
+        tabBarIcon: ({ focused }) => (
+          <Ionicons
+            name="ios-list"
+            size={12}
+            color={focused ? "#000" : "#ccc"}
+          />
+        ),
+      }}
+    />
+
+    <Tabs.Screen
+      name="home/index"
+      options={{
+        headerShown: false,
+        tabBarLabel: "Anasayfa",
+        tabBarIcon: ({ focused }) => (
+          <Ionicons
+            name="ios-home"
+            size={24}
+            color={focused ? "#000" : "#ccc"}
+          />
+        ),
+      }}
+    />
+    <Tabs.Screen
+      name="branches/index"
+      options={{
+        headerShown: false,
+        tabBarLabel: "Şubeler",
+        tabBarIcon: ({ focused }) => (
+          <Ionicons
+            name="ios-pin"
+            size={24}
+            color={focused ? "#000" : "#ccc"}
+          />
+        ),
+      }}
+    />
+    <Tabs.Screen
+      name="profiles/index"
+      options={{
+        headerShown: false,
+        tabBarLabel: "Profilim",
+        tabBarIcon: ({ focused }) => (
+          <Ionicons
+            name="ios-person"
+            size={24}
+            color={focused ? "#000" : "#ccc"}
+          />
+        ),
+      }}
+    />
+  </Tabs>
+);
 
 export default TabsLayout;
--- app/(tabs)/home/index.jsx
+++ app/(tabs)/home/index.jsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 const Homepage = () => {
   return (
     <View>
-      <Text>Homepage</Text>
+      <Text></Text>
     </View>
   );
 };
